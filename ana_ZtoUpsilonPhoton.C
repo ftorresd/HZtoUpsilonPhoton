404d089daf5b006b1c95acec43a70ec343e1f776
Felipe Silva <felipe.silva@cern.ch>
2017-10-24 17:11:09 +0200

    a

---
+++ ana_ZtoUpsilonPhoton.C
@@ -0,0 +1,20 @@
+
+// Z --> Upsilon + Photon
+
+// Analysis Code
+// Description:  - Reads the ggNtuples
+//               - Skim the sample
+//               - Apply corrections and cuts
+//               - Produces a ntuple read for plotting
+
+
+
+
+#include "plugins/untuplizer_v07.h"
+
+
+void ana_ZtoUpsilonPhoton(const char* Inpath, TString outpath)  
+{
+    TreeReader data(Inpath);
+
+}
